af1e033fbdcf3ebba4ba2e2fae1770a3cf317c22
Jon Smith <aardvarkjon94@yahoo.com>
2021-10-29 21:20:39 -0500

    Setup templates

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,9 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 
 def index():
     user = {'username': "Jon"}
-    return '''
-    <html>
-        <head>
-            <title>Home Page - MicroBlog</title>
-        </head>
-        <body>
-            <h1>Hello, ''' + user['username'] + '''!</h1>
-        </body>
-    </html>
-    '''
+    return render_template('index.html', title="home", user=user)
